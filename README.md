e98083ed59e56da760bfc0a6bc62070ff862da20
Harry Gale <harry.n.gale@gmail.com>
2016-12-01 16:17:14 -0800

    iOS homework

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ios-homework
+# Eaze iOS Homework
+
+>Our iOS code challenge for engineering applicants
+
+## Project
+
+Using the [Giphy API](https://github.com/Giphy/GiphyAPI), implement a gif search app. This is an open ended project so do as much or as little as you think is necessary to achieve the desired goal.
+
+## Structure
+
+Do not use interface builder to make this app. If you want to complete this project using React Native then that is also an option. This app should work with iOS 9+.
+
---
+++ iOSHomeWork/iOSHomeWorkUITests/iOSHomeWorkUITests.m
@@ -0,0 +1,40 @@
+//
+//  iOSHomeWorkUITests.m
+//  iOSHomeWorkUITests
+//
+//  Created by Harry Tormey on 12/1/16.
+//  Copyright © 2016 Harry Tormey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSHomeWorkUITests : XCTestCase
+
+@end
+
+@implementation iOSHomeWorkUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
